c9c98e1eee7a1b2bfce1eebe9744ba9bdd6fafce
Jakub Korsak <jakub.korsak@protonmail.com>
2022-11-17 12:52:29 +0000

    add go version, move python to python/ dir

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/korsakjakub/scattering/v2
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 2.0
+	s := "stinrg"
+	a += 3
+	s += "xD"
+	fmt.Println("hello, world")
+}
