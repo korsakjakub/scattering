bb21910be1d1841d13c4ea070ea56a08fef1e23a
Jakub Korsak <jakub.korsak@protonmail.com>
2022-11-19 23:59:49 +0100

    wf looks properly

--- go/main.go
+++ go/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/wcharczuk/go-chart"
+	"github.com/wcharczuk/go-chart/v2"
 	"math"
 	"os"
 )
 
 const De = 0.0011141
 const Re = 10.98
-const angularMomentum = 3.0
+const angularMomentum = 0.0
 
 func kSq(position, mu, collisionEnergy float64) float64 {
 	potential := func(position float64) float64 {
@@ -32,29 +32,28 @@ func reducedMass(m1, m2 float64) float64 {
 }
 
 func main() {
-	ratio := 10.0
+	ratio := 100.0
 	u := 1822.888
 	m39k := 38.963703 * u
 	m40k := 39.963999 * u
 	// m41k := 40.961825 * 1822.888
 	initialPosition := 10.0
 	initialPhi := 1.0
-	collisionEnergy := 1.0
-	indexRange := 200
+	collisionEnergy := math.Pow(10, -8)
+	indexRange := 50000
 
 	mu := reducedMass(m39k, m40k)
 	gridStep := 2 * math.Pi / (50 * math.Sqrt(kSq(Re, mu, collisionEnergy)))
 
 	var k []float64
 	for i := 0; i <= indexRange+1; i += 1 {
-		k = append(k, kSq(initialPosition, mu, collisionEnergy))
+		k = append(k, kSq(initialPosition+float64(i)*gridStep, mu, collisionEnergy))
 	}
 
 	var ratios []float64
 	for i := 1; i <= indexRange; i += 1 {
 		ratio = waveFunctionRatioGetN(ratio, i, gridStep, k)
 		ratios = append(ratios, ratio)
-		fmt.Println(ratio)
 	}
 
 	var phi []float64
@@ -69,15 +68,29 @@ func main() {
 		x[i] = float64(i + low)
 	}
 	graph := chart.Chart{
+		XAxis: chart.XAxis{
+			Name: "R",
+		},
+		YAxis: chart.YAxis{
+			Name: "wf",
+		},
 		Series: []chart.Series{
 			chart.ContinuousSeries{
+				Style: chart.Style{
+					StrokeColor: chart.GetDefaultColor(0).WithAlpha(255),
+				},
 				XValues: x,
 				YValues: phi,
 			},
 		},
 	}
 	f, _ := os.Create("output.png")
-	defer f.Close()
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(f)
 	err := graph.Render(chart.PNG, f)
 	if err != nil {
 		fmt.Println(err)
